92154e3bc8efe29087642b7ba42aca2af3c2265d
Sam Smith <smithrunner@live.com>
2017-09-20 14:54:03 +0000

    mentor availbility method added

--- lib/kele.rb
+++ lib/kele.rb
@@ -17,6 +17,12 @@ class Kele
   def get_me
     response = self.class.get(api_end_point("users/me"), headers: {"authorization" => @auth_token})
     @user_data = JSON.parse(response.body)
+    puts "Your mentor's ID is: #{@user_data['current_enrollment']['mentor_id']}"
+  end
+  
+  def get_mentor_availability(mentor_id)
+    response = self.class.get(api_end_point("mentors/#{mentor_id}/student_availability"), headers: {"authorization" => @auth_token})
+    @mentor_availability = JSON.parse(response.body)
   end
   
   private
